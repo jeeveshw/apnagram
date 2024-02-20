fd52df9bf3814a3f66b6cc3adbeaa06e2bebde4b
jeevesh <28839382+jeeveshw@users.noreply.github.com>
2024-02-20 23:52:48 +0530

    appwrite  api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,3 +1,21 @@
-export async function createUserAccount(user) {
+import { ID } from "appwrite"
+import { INewUser } from "@/types";
+import { error } from "console";
+import { account } from "./config";
+
+export async function createUserAccount(user: INewUser) {
     
+    try {
+
+        const newAccount = await account.create (
+                ID.unique(),
+                user.name,
+                user.email,
+                user.password
+        );
+    } catch(error) {
+        console.log(error)
+        return error;
+
+    } 
 }
