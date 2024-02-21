9d2a978dabe3228cf21c4a104949a1dd8d04dbff
jeevesh <28839382+jeeveshw@users.noreply.github.com>
2024-02-22 03:23:34 +0530

    get current user session

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom";
+import { getCurrentUser } from '@/lib/appwrite/api';
 import { IContextType, IUser } from '@/types';
 import{createContext, useContext, useEffect, useState} from 'react';
 
 
 export const INITIAL_USER ={
-    id: '',
+    id: ' ',
     name: '',
     username: '',
     email: '',
@@ -24,6 +26,7 @@ export const INITIAL_STATE = {
 const AuthContext = createContext<IContextType>(INITIAL_STATE)
 
 const AuthProvider = ({ children }: {children: React.ReactNode }) => {
+       const navigate = useNavigate();
         const [user, setUser] = useState<IUser>(INITIAL_USER)
         const [isLoading, setIsLoading] = useState(false);
         const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -31,6 +34,21 @@ const AuthProvider = ({ children }: {children: React.ReactNode }) => {
         const checkAuthUser = async () =>{
             try {
                 const currentAccount = await getCurrentUser();
+                if (currentAccount) {
+                    setUser({
+                      id: currentAccount.$id,
+                      name: currentAccount.name,
+                      username: currentAccount.username,
+                      email: currentAccount.email,
+                      imageUrl: currentAccount.imageUrl,
+                      bio: currentAccount.bio,
+                    });
+                    setIsAuthenticated(true);
+            
+                    return true;
+                  }
+            
+                  return false;
             } catch(error) {
                 console.log(error);
                 return false;
