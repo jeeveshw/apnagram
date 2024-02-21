e2c536eb4164d7d20f0148832e4939be8006804c
jeevesh <28839382+jeeveshw@users.noreply.github.com>
2024-02-21 23:47:00 +0530

    configuration

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -4,7 +4,12 @@ export const appwriteConfig = {
     url: import.meta.env.VITE_APPWRITE_URL,
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
     databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVE_COLLECTION_ID,
     
+
 
 
 }
